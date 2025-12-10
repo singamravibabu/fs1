d8bb0e55bfb59f46bac3fe99fd9d410d538f18ec
Singam Ravi Babu <singam.ravibabu@gmail.com>
2025-12-10 17:18:46 +0530

    Dec 10

--- temperature.py
+++ temperature.py
@@ -1,7 +1,13 @@
+'''This file contains function to convert from one unit to another.
+As of now it contains only one function.
+However, we will add more functions in the future.'''
+
 def to_fahrenheit(celsius):
+    'This function converts celsius to fahrenheit.'
     fahrenheit = (celsius * 9/5) + 32
     return fahrenheit
 
 def to_celsius(fahrenheit):
+    'This function converts fahrenheit to celsius.'
     celsius = (fahrenheit - 32) * 5 / 9
     return celsius
